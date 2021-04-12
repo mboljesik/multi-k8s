a0081605aab19261dfbc95ab9854a245c9a79b75
michal.boljesik <michal.boljesik@synpulse.com>
2021-04-12 15:32:14 +0200

    Skaffold integration

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          Fib Calculator version KUBERNETES!
+          Fib Calculator UPDATED AGAIN!
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
